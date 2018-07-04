45fbf52438d65a49e4808a6a0e046e042c9b7d35
mzaini30 <muhzaini30@gmail.com>
2018-07-04 18:17:46 +0800

    oke

--- bin/app.js
+++ bin/app.js
@@ -8,9 +8,12 @@ $('.input').on('keyup', function(){
 		input = input.replace(new RegExp(awal, 'g'), akhir)
 	}
 
-	karakter_awal = ['#1A', '#2A', '#4A', '#5A', '#6A', '#1B', '#5a', '#6a', '1A', '2A', '3A', '4A', '5A', '6A', '7A', '1B', '5a', '6a', '7a', '#1', '#2', '#4', '#5', '#6', '1', '2', '3', '4', '5', '6', '7']
+	karakter_awal = ['1A#', '2A#', '4A#', '5A#', '6A#', '1B#', '5a#', '6a#', '1A', '2A', '3A', '4A', '5A', '6A', '7A', '1B', '5a', '6a', '7a', '1#', '2#', '4#', '5#', '6#', '1', '2', '3', '4', '5', '6', '7']
 
 	karakter_akhir = ['l', '\'', 'e', 't', 'u', 'p', 'x', 'v', 'k', ';', 'q', 'w', 'r', 'y', 'i', 'o', 'z', 'c', 'b', 'm', '.', 's', 'f', 'h', 'n', ',', '/', 'a', 'd', 'g', 'j']
+
+	ubah(' ', '')
+	ubah('\n', ' ')
 
 	for (n in karakter_awal){
 		ubah(karakter_awal[n], karakter_akhir[n])
